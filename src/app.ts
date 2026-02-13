cdf77f94448832dae6ea2941f0e1feca744b2d3b
Kazi Irfan <irfanofficial.work@gmail.com>
2026-02-13 23:35:05 +0600

    test 05

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.get("/", (req: Request, res: Response) => {
   return res.status(httpStatus.OK).json({
     success: true,
     code: httpStatus.OK,
-    message: "Final Server is running ... ok irfan success All is ok",
+    message: "Server is running ... check the changes",
     availablePath: moduleRoutes,
   });
 });
